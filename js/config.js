4e84c14040417ba09c4c1ce0a04288db4148e751
paulorobertoxavierjunior-create <paulorobertoxavierjunior@gmail.com>
2026-04-11 22:06:17 -0400

    Criar js/config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window.ELAYON_CONFIG = {
+    supabase: {
+        url: "https://eudcjihffrfmhzmfwtlg.supabase.co",
+        anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1ZGNqaWhmZnJmbWh6bWZ3dGxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3NDE3MjUsImV4cCI6MjA5MDMxNzcyNX0.2tod6vvl_4SAXzSmW1wU8Mk9pLn8fvhF2xrAZOysUu0"
+    },
+    routes: {
+        login: "login.html",
+        painel: "https://paulorobertoxavierjunior-create.github.io/elayon-presenca/painel.html" // Rota externa para o outro repo
+    }
+};
