226adb329bfe9e53fee73f61e778e218cd9240cb
paulorobertoxavierjunior-create <paulorobertoxavierjunior@gmail.com>
2026-04-12 16:49:46 -0400

    Atualizar o config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,17 @@
 // CONFIGURAÇÃO CORE ELAYON - REPOSITÓRIO: elayon-cadastro
 window.ELAYON_CONFIG = {
-    supabase: {
-        url: "https://eudcjihffrfmhzmfwtlg.supabase.co",
-        anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1ZGNqaWhmZnJmbWh6bWZ3dGxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3NDE3MjUsImV4cCI6MjA5MDMxNzcyNX0.2tod6vvl_4SAXzSmW1wU8Mk9pLn8fvhF2xrAZOysUu0"
-    },
-    routes: {
-        login: "login.html",
-        cadastro: "cadastro.html",
-        // URL final após o e-mail de confirmação ou login bem-sucedido
-        painel: "https://paulorobertoxavierjunior-create.github.io/elayon-cadastro/painel.html"
-    }
+  supabase: {
+    url: "https://eudcjihffrfmhzmfwtlg.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1ZGNqaWhmZnJmbWh6bWZ3dGxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3NDE3MjUsImV4cCI6MjA5MDMxNzcyNX0.2tod6vvl_4SAXzSmW1wU8Mk9pLn8fvhF2xrAZOysUu0"
+  },
+  routes: {
+    index: "index.html",
+    login: "login.html",
+    cadastro: "cadastro.html",
+    painel: "https://paulorobertoxavierjunior-create.github.io/elayon-cadastro/painel.html",
+    presenca: "https://paulorobertoxavierjunior-create.github.io/elayon-presenca/"
+  },
+  storage: {
+    userKey: "elayon_user"
+  }
 };
